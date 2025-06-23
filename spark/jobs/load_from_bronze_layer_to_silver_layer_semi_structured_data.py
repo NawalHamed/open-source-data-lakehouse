a9dbe13aa307f75d59ecd93d8b0646cf4273544c
Nawal Hamed Al Mawali <65490811+NawalHamed@users.noreply.github.com>
2025-06-23 12:03:29 +0400

    Update load_from_bronze_layer_to_silver_layer_semi_structured_data.py

--- spark/jobs/load_from_bronze_layer_to_silver_layer_semi_structured_data.py
+++ spark/jobs/load_from_bronze_layer_to_silver_layer_semi_structured_data.py
@@ -3,6 +3,7 @@ from pyspark.sql import SparkSession
 # Step 1: Create SparkSession (with URI fix to /api/v2)
 spark = SparkSession.builder \
     .appName("Airline JSON to Iceberg Silver") \
+    .master("spark://spark-master:7077") \
     .config("spark.sql.catalog.nessie", "org.apache.iceberg.spark.SparkCatalog") \
     .config("spark.sql.catalog.nessie.catalog-impl", "org.apache.iceberg.nessie.NessieCatalog") \
     .config("spark.sql.catalog.nessie.uri", "http://nessie:19120/api/v1") \
