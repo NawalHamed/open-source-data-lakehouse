1fdc50466ae8aebf753342e75e3d0c5eb323125d
Nawal Hamed Al Mawali <65490811+NawalHamed@users.noreply.github.com>
2025-07-02 16:24:40 +0400

    Update load_from_bronze_layer_to_silver_layer_all_semi_structured_data.py

--- spark/jobs/load_from_bronze_layer_to_silver_layer_all_semi_structured_data.py
+++ spark/jobs/load_from_bronze_layer_to_silver_layer_all_semi_structured_data.py
@@ -50,9 +50,9 @@ spark.sql("CREATE NAMESPACE IF NOT EXISTS nessie.silver_layer")
 
 # Step 5: Write to Iceberg Silver layer tables
 
-df_airline_clean.writeTo("nessie.silver_layer.airline_data").createOrReplace()
-df_airport_clean.writeTo("nessie.silver_layer.airport_data").createOrReplace()
-df_flight_clean.writeTo("nessie.silver_layer.flight_data").createOrReplace()
+#df_airline_clean.writeTo("nessie.silver_layer.airline_data").createOrReplace()
+#df_airport_clean.writeTo("nessie.silver_layer.airport_data").createOrReplace()
+#df_flight_clean.writeTo("nessie.silver_layer.flight_data").createOrReplace()
 
 
 df_airline_clean.writeTo("nessie.silver_layer.airline_data").append()
