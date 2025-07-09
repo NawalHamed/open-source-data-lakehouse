b8059eb37ea76d42707f995572bee058ad274129
Nawal Hamed Al Mawali <65490811+NawalHamed@users.noreply.github.com>
2025-07-09 10:12:44 +0400

    Add files via upload

---
+++ airflow/dags/load_unstructured_metadata_data_from_bronze_to_silver_table.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 1, 1),
+}
+
+with DAG(
+    dag_id="load_unstratutured_metadata_data_from_bronze_to_silver",
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    run_spark_job = BashOperator(
+        task_id="run_spark_job",
+        bash_command="docker exec spark-master spark-submit /opt/spark_jobs/load_from_bronze_layer_to_silver_layer_unstructured_data.py",
+    )
