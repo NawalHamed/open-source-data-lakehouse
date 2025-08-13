8771c8615bdec361213ff37068c1f4f7176a94b9
Nawal Hamed Al Mawali <65490811+NawalHamed@users.noreply.github.com>
2025-08-13 09:26:10 +0400

    Update load_from_silver_to_gold_layer_summary_data.py

--- airflow/dags/load_from_silver_to_gold_layer_summary_data.py
+++ airflow/dags/load_from_silver_to_gold_layer_summary_data.py
@@ -10,11 +10,13 @@ default_args = {
 with DAG(
     dag_id="load_from_silver_layer_to_gold_layer_summary_data",
     default_args=default_args,
-    schedule_interval=None,
+    schedule_interval="@daily", # Run once every day
     catchup=False,
 ) as dag:
-
+    
+    # Task: Run Spark job inside Docker container
     run_spark_job = BashOperator(
         task_id="run_spark_job",
+        # Command: Run 'spark-submit' inside 'spark-master' container
         bash_command="docker exec spark-master spark-submit /opt/spark_jobs/load_from_silver_layer_to_gold_layer_summary_data.py",
     )
