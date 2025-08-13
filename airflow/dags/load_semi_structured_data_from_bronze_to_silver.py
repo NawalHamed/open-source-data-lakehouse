1dab5e1af43bb6cfc77acda464f1f32eb9a06042
Nawal Hamed Al Mawali <65490811+NawalHamed@users.noreply.github.com>
2025-08-13 09:33:53 +0400

    Update load_semi_structured_data_from_bronze_to_silver.py

--- airflow/dags/load_semi_structured_data_from_bronze_to_silver.py
+++ airflow/dags/load_semi_structured_data_from_bronze_to_silver.py
@@ -2,11 +2,14 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+# ========== Default Arguments for the DAG ==========
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2024, 1, 1),
 }
 
+
+# ========== Define the DAG ==========
 with DAG(
     dag_id="load_semi_structured_data_from_bronze_to_silver",
     default_args=default_args,
@@ -14,7 +17,13 @@ with DAG(
     catchup=False,
 ) as dag:
 
+    # ========== Task: Run Spark Job ==========
     run_spark_job = BashOperator(
         task_id="run_spark_job_2",
+        
+        # Command to:
+        # 1. Enter the 'spark-master' Docker container
+        # 2. Run 'spark-submit' to start a PySpark job
+        # 3. Execute the ETL script for moving data from Bronze → Silver layer
         bash_command="docker exec spark-master spark-submit /opt/spark_jobs/load_from_bronze_layer_to_silver_layer_all_semi_structured_data.py",
     )
