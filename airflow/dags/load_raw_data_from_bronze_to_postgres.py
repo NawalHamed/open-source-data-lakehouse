34296ed6bf10a12ba438eadd7e11aab2537d12d7
Nawal Hamed Al Mawali <65490811+NawalHamed@users.noreply.github.com>
2025-08-13 09:29:47 +0400

    Update load_raw_data_from_bronze_to_postgres.py

--- airflow/dags/load_raw_data_from_bronze_to_postgres.py
+++ airflow/dags/load_raw_data_from_bronze_to_postgres.py
@@ -1,3 +1,5 @@
+# This DAG is for testing purpose only
+
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
@@ -10,12 +12,20 @@ default_args = {
 with DAG(
     dag_id="load_raw_data_from_bronze_to_postgres",
     default_args=default_args,
-    schedule_interval=None,
+    schedule_interval=None, # No automatic schedule (manual trigger only)
     catchup=False,
 ) as dag:
 
+    # Task: Run Spark job that loads raw data from Bronze Layer into PostgreSQL
     run_spark_job = BashOperator(
         task_id="run_spark_job_2",
+        
+        # Bash command to:
+        # 1. Execute into 'spark-master' container
+        # 2. Run 'spark-submit' to submit the job to Spark
+        # 3. Specify Spark master URL for cluster mode
+        # 4. Include PostgreSQL JDBC driver so Spark can connect to Postgres
+        # 5. Run the Python ETL script inside /opt/spark_jobs/
         bash_command=(
             "docker exec spark-master spark-submit "
             "--master spark://spark-master:7077 "
